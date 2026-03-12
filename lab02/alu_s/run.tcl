b3441af37932e7a0d6fd0545ec79dc1fe9eb7750
XM Zheng <ximingzheng@smail.nju.edu.cn>
2026-03-12 23:36:03 +0800

    Add Makefile

--- lab02/alu_s/run.tcl
+++ lab02/alu_s/run.tcl
@@ -1,12 +1,12 @@
-set part xc7a100tcsg324-1
-set outputDir ./build
+set PART xc7a100tcsg324-1
+set BUILD_DIR ./build
 
 read_verilog [glob ./*.v]
 read_xdc [glob ./*.xdc]
 
-synth_design -top top -part $part
+synth_design -top top -part $PART
 opt_design
 place_design
 route_design
 
-write_bitstream -force $outputDir/top.bit
+write_bitstream -force $BUILD_DIR/top.bit
