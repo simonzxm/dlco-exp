31b708ff3095f9b6e5b4b78b7c4218f99ede3f3d
sprite <ximingzheng@smail.nju.edu.cn>
2026-03-13 16:10:29 +0800

    Update lab03/timer

--- lab03/timer/timer.v
+++ lab03/timer/timer.v
@@ -1,13 +1,29 @@
 module timer (
     input clk,
+    input start,
+    input pause,
+    input reset,
     output reg [7:0] second
 );
 
+  reg running;
+
   always @(posedge clk) begin
-    if (second[3:0] == 9) begin
-      second[3:0] <= 0;
-      second[7:4] <= (second[7:4] == 5) ? 0 : (second[7:4] + 1);
-    end else second <= second + 1;
+    if (reset) begin
+      second  <= 8'd0;
+      running <= 0;
+    end else if (start) begin
+      running <= 1;
+    end else if (pause) begin
+      running <= 0;
+    end else if (running) begin
+      if (second[3:0] == 9) begin
+        second[3:0] <= 0;
+        second[7:4] <= (second[7:4] == 5) ? 0 : (second[7:4] + 1);
+      end else begin
+        second <= second + 1;
+      end
+    end
   end
 
 endmodule
--- lab03/timer/top.v
+++ lab03/timer/top.v
@@ -1,11 +1,36 @@
 module top (
     input CLK100MHZ,
+    input BTNL,
+    input BTNC,
+    input BTNR,
     output [6:0] SEG,
     output [7:0] AN
 );
 
   wire clk;
   wire [7:0] second;
+
+  wire start;
+  wire pause;
+  wire reset;
+
+  button start_btn (
+      .clk(CLK100MHZ),
+      .btn(BTNL),
+      .btn_edge(start)
+  );
+
+  button pause_btn (
+      .clk(CLK100MHZ),
+      .btn(BTNC),
+      .btn_edge(pause)
+  );
+
+  button reset_btn (
+      .clk(CLK100MHZ),
+      .btn(BTNR),
+      .btn_edge(reset)
+  );
 
   clk_1s clock (
       .clk(CLK100MHZ),
@@ -14,7 +39,10 @@ module top (
 
   timer seconds (
       .clk(clk),
-      .second(second)
+      .second(second),
+      .start(start),
+      .pause(pause),
+      .reset(reset)
   );
 
   display numbers (
