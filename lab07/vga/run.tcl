da60c12ec28f90b1cb18d651f87a0b213100a140
sprite <ximingzheng@smail.nju.edu.cn>
2026-04-17 14:51:59 +0800

    Update lab07/vga

---
+++ lab07/vga/run.tcl
@@ -0,0 +1,40 @@
+set PART xc7a100tcsg324-1
+set BUILD_DIR ./build
+
+# Create in-memory project and generate IP
+create_project -in_memory -part $PART
+create_ip -name blk_mem_gen -vendor xilinx.com -library ip -version 8.4 \
+    -module_name image_rom -dir $IP_DIR
+set_property -dict [list \
+    CONFIG.Memory_Type {Single_Port_ROM} \
+    CONFIG.Write_Width_A {12} \
+    CONFIG.Write_Depth_A {307200} \
+    CONFIG.Read_Width_A {12} \
+    CONFIG.Enable_A {Always_Enabled} \
+    CONFIG.Register_PortA_Output_of_Memory_Primitives {false} \
+    CONFIG.Load_Init_File {true} \
+    CONFIG.Coe_File [file normalize ./image.coe] \
+] [get_ips image_rom]
+generate_target all [get_ips image_rom]
+synth_ip [get_ips image_rom]
+
+# Generate Clocking Wizard IP
+create_ip -name clk_wiz -vendor xilinx.com -library ip -version 6.0 -module_name clk_wiz_0 -dir $IP_DIR
+set_property -dict [list \
+    CONFIG.PRIM_IN_FREQ {100.000} \
+    CONFIG.CLKOUT1_REQUESTED_OUT_FREQ {25.000} \
+    CONFIG.USE_LOCKED {false} \
+    CONFIG.USE_RESET {false} \
+] [get_ips clk_wiz_0]
+generate_target all [get_ips clk_wiz_0]
+synth_ip [get_ips clk_wiz_0]
+
+read_verilog [glob ./*.v]
+read_xdc [glob ./*.xdc]
+
+synth_design -top top -part $PART
+opt_design
+place_design
+route_design
+
+write_bitstream -force $BUILD_DIR/top.bit
---
+++ lab07/vga/top.v
@@ -0,0 +1,47 @@
+module top (
+    input CLK100MHZ,
+    input CPU_RESETN,
+    output [3:0] VGA_R,
+    output [3:0] VGA_G,
+    output [3:0] VGA_B,
+    output VGA_HS,
+    output VGA_VS
+);
+
+  wire clk_25mhz;
+
+  clk_wiz_0 u_clk_wiz (
+      .clk_in1 (CLK100MHZ),
+      .clk_out1(clk_25mhz)
+  );
+
+  wire [9:0] h_addr;
+  wire [9:0] v_addr;
+  wire valid;
+  wire [11:0] vga_data;
+  wire [18:0] rom_addr;
+
+  assign rom_addr = h_addr + v_addr * 10'd640;
+
+  image_rom image_rom_inst (
+      .clka (clk_25mhz),
+      .addra(rom_addr),
+      .douta(vga_data)
+  );
+
+  vga_ctrl vga_ctrl_inst (
+      .pclk(clk_25mhz),
+      .reset(CPU_RESETN),
+      .vga_data(vga_data),
+      .h_addr(h_addr),
+      .v_addr(v_addr),
+      .hsync(VGA_HS),
+      .vsync(VGA_VS),
+      .valid(valid),
+      .vga_r(VGA_R),
+      .vga_g(VGA_G),
+      .vga_b(VGA_B)
+  );
+
+endmodule
+
