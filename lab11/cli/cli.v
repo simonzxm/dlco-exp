95993d612bbb6916f2ce18294929c8df7715aafd
sprite <ximingzheng@smail.nju.edu.cn>
2026-05-29 15:57:17 +0800

    Update lab11/cli

--- lab11/cli/cli.v
+++ lab11/cli/cli.v
@@ -15,20 +15,25 @@ module cli (
   reg [4:0] cursor_y = 0;
   reg [7:0] prev_key_count = 0;
   reg offset = 0;
+  reg [2399:0] vram_valid = 0;
+  wire [4:0] next_y = (cursor_y == 29) ? 0 : cursor_y + 1;
 
   always @(posedge clk) begin
     if (key_count != prev_key_count) begin
       prev_key_count <= key_count;
       if (ascii_key == 8'h0D) begin
         cursor_x <= 0;
-        cursor_y <= (cursor_y == 29) ? 0 : cursor_y + 1;
+        cursor_y <= next_y;
         if (cursor_y == 29) offset <= 1;
+        vram_valid[next_y*80+:80] <= 80'b0;
       end else if ((ascii_key >= 8'h20) && (ascii_key <= 8'h7E)) begin
         vram[(cursor_y*80)+cursor_x] <= ascii_key;
+        vram_valid[(cursor_y*80)+cursor_x] <= 1'b1;
         if (cursor_x == 79) begin
           cursor_x <= 0;
-          cursor_y <= (cursor_y == 29) ? 0 : cursor_y + 1;
+          cursor_y <= next_y;
           if (cursor_y == 29) offset <= 1;
+          vram_valid[next_y*80+:80] <= 80'b0;
         end else begin
           cursor_x <= cursor_x + 1;
         end
@@ -36,12 +41,10 @@ module cli (
     end
   end
 
-  wire [5:0] y_sum = v_addr[9:4] + cursor_y + 6'd1;
-  wire [4:0] vram_row = offset ? ((y_sum >= 6'd30) ? (y_sum - 6'd30) : y_sum) : v_addr[9:4];
-
-  wire is_blank = (vram_row == cursor_y && h_addr[9:3] >= cursor_x) || (!offset && vram_row > cursor_y);
-  wire [7:0] char_code = is_blank ? 8'h20 : vram[(vram_row*80)+h_addr[9:3]];
-
+  wire [ 5:0] y_sum = v_addr[9:4] + cursor_y + 6'd1;
+  wire [ 4:0] vram_row = offset ? ((y_sum >= 6'd30) ? (y_sum - 6'd30) : y_sum[4:0]) : v_addr[9:4];
+  wire [11:0] vram_addr = (vram_row * 80) + h_addr[9:3];
+  wire [ 7:0] char_code = vram_valid[vram_addr] ? vram[vram_addr] : 8'h20;
   wire [11:0] font_row = font_rom[{char_code, v_addr[3:0]}];
   assign vga_data = font_row[h_addr[2:0]] ? 12'hFFF : 12'h000;
 
