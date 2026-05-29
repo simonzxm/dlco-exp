49dd9c3bcbe0cccea715be06858ffd3f9255778d
sprite <ximingzheng@smail.nju.edu.cn>
2026-05-29 15:13:54 +0800

    Update lab11/cli

--- lab11/cli/cli.v
+++ lab11/cli/cli.v
@@ -14,6 +14,7 @@ module cli (
   reg [6:0] cursor_x = 0;
   reg [4:0] cursor_y = 0;
   reg [7:0] prev_key_count = 0;
+  reg [4:0] y_offset = 0;
 
   always @(posedge clk) begin
     if (key_count != prev_key_count) begin
@@ -21,9 +22,11 @@ module cli (
       if (ascii_key == 8'h0D) begin
         cursor_x <= 0;
         cursor_y <= (cursor_y == 29) ? 0 : cursor_y + 1;
+        y_offset <= (cursor_y == 29) ? y_offset + 1 : y_offset;
       end else if (cursor_x == 79) begin
         cursor_x <= 0;
         cursor_y <= (cursor_y == 29) ? 0 : cursor_y + 1;
+        y_offset <= (cursor_y == 29) ? y_offset + 1 : y_offset;
         vram[(cursor_y*80)+cursor_x] <= ascii_key;
       end else begin
         cursor_x <= cursor_x + 1;
@@ -32,7 +35,7 @@ module cli (
     end
   end
 
-  wire [11:0] font_row = font_rom[{vram[(v_addr[9:4]*80)+h_addr[9:3]], v_addr[3:0]}];
+  wire [11:0] font_row = font_rom[{vram[((v_addr[9:4]+y_offset)*80)+h_addr[9:3]], v_addr[3:0]}];
   assign vga_data = font_row[h_addr[2:0]] ? 12'hFFF : 12'h000;
 
 endmodule
